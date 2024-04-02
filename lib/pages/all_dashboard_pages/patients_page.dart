c8aa2404ab6d5a9e606406331f559488b1607d9c
HitbyaParkedCar <gradius48@gmail.com>
2024-04-02 11:14:17 -0700

    Added a barebones frame for entering patient information.

--- lib/pages/all_dashboard_pages/patients_page.dart
+++ lib/pages/all_dashboard_pages/patients_page.dart
@@ -1,32 +1,385 @@
 import 'package:flutter/material.dart';
 
 class PatientList extends StatefulWidget {
-  const PatientList({super.key});
+  const PatientList({Key? key}) : super(key: key);
 
   @override
   _PatientListState createState() => _PatientListState();
 }
 
 class _PatientListState extends State<PatientList> {
+  final TextEditingController _searchController = TextEditingController();
+  final List<String> _patients = [];
+  List<String> _searchResults = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Patients'),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => AddPatientPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: _onSearchTextChanged,
+              decoration: InputDecoration(
+                labelText: 'Search Patients',
+                suffixIcon: IconButton(
+                  icon: const Icon(Icons.clear),
+                  onPressed: () {
+                    _searchController.clear();
+                    _onSearchTextChanged('');
+                  },
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _searchResults.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_searchResults[index]),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onSearchTextChanged(String value) {
+    _searchResults.clear();
+    if (value.isEmpty) {
+      setState(() {});
+      return;
+    }
+
+    _patients.forEach((patient) {
+      if (patient.toLowerCase().contains(value.toLowerCase())) {
+        _searchResults.add(patient);
+      }
+    });
+
+    setState(() {});
+  }
+}
+
+class AddPatientPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Personal Information'),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'First Name:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter first name',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Middle Name:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter middle name',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Last Name:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter last name',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Date of Birth (YYYY/MM/DD):',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter date of birth',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Blood Group:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter blood group',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'RH Factor:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter RH factor',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Marital Status:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter marital status',
+                ),
+              ),
+              SizedBox(height: 12),
+              Text(
+                'Preferred Language:',
+                style: TextStyle(fontSize: 16),
+              ),
+              TextField(
+                decoration: InputDecoration(
+                  hintText: 'Enter preferred language',
+                ),
+              ),
+              SizedBox(height: 16),
+              Align(
+                alignment: Alignment.centerRight,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondPage()),
+                    );
+                  },
+                  child: Text('Next'),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      body: const Center(
-        child: Text(
-          'Patients',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contact Information'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Home Phone:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter home phone',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Phone:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter phone number',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Email Address:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter email address',
+              ),
+            ),
+            SizedBox(height: 24),
+            Text(
+              'Emergency Contact',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'First Name:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter first name',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Last Name:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter last name',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Relationship:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter relationship',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Phone Number:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter phone number',
+              ),
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => ThirdPage()),
+                    );
+                  },
+                  child: Text('Next'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Medical History'),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Known Medical Illnesses:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter known medical illnesses',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Previous Medical Illnesses:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter previous medical illnesses',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Allergies:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter allergies',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Current Medications:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter current medications',
+              ),
+            ),
+            SizedBox(height: 12),
+            Text(
+              'Past Medications:',
+              style: TextStyle(fontSize: 16),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: 'Enter past medications',
+              ),
+            ),
+            SizedBox(height: 16),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => PatientList()),
+                    );
+                  },
+                  child: Text('Finish'),
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
